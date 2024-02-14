b596b7c20d45801cf9ffabeb4fb8d70466de3e64
Webster <strategywebstermanyanga@gmail.com>
2024-02-14 10:35:45 +0100

    checkpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent
+  }
+];
 
